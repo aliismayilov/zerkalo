578cc7f74d6dd301d5f8693cc044d934efd03810
Ali Ismayilov <ali@ismailov.info>
2011-07-14 13:43:53 +0500

    updated imageflow configuration

--- web and desktop/zerkalo_web/editions/static/imageflow/imageflow.packed.js
+++ web and desktop/zerkalo_web/editions/static/imageflow/imageflow.packed.js
@@ -40,5 +40,10 @@ eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a
 domReady(function()
 {
 	var instanceOne = new ImageFlow();
-	instanceOne.init({ ImageFlowID:'myImageFlow' });
+	instanceOne.init({
+        ImageFlowID:'myImageFlow',
+        reflections: false, 
+        reflectionP: 0.0,
+        opacity: true,
+        imageCursor: 'pointer',
 });
