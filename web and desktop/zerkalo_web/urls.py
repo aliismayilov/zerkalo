670bbda67cb2d9ce99ac3deddca49d1c0c1e9752
Ali Ismayilov <ali@ismailov.info>
2011-07-22 13:32:45 +0500

    mobile versions are ready, now they are based on javascript answer, redirect to /m or /m1

--- web and desktop/zerkalo_web/urls.py
+++ web and desktop/zerkalo_web/urls.py
@@ -5,9 +5,16 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('editions.views',
-    (r'^$', 'show'),
+    (r'^$', 'show'), # desktop
+    (r'^m$', 'show_m'), # advanced mobile
+    (r'^m1$', 'show_m1'), # old mobile
+    
     (r'^(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})/$', 'show'),
-    (r'^search.html', 'search')
+    (r'^m/(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})/$', 'show_m'),
+    (r'^m1/(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})/$', 'show_m1'),
+    
+    (r'^search.html', 'search'),
+    (r'^m/search.html', 'search_m')
 )
 
 urlpatterns += patterns('',
