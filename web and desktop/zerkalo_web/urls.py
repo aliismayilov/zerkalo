07ad694b2fb60951b046cd13e7876b5d060f0292
Ali Ismayilov <ali@ismailov.info>
2011-07-23 14:00:51 +0500

    fixed urls.py

--- web and desktop/zerkalo_web/urls.py
+++ web and desktop/zerkalo_web/urls.py
@@ -6,8 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('editions.views',
     (r'^$', 'show'), # desktop
-    (r'^m$', 'show_m'), # advanced mobile
-    (r'^m1$', 'show_m1'), # old mobile
+    (r'^m/$', 'show_m'), # advanced mobile
+    (r'^m1/$', 'show_m1'), # old mobile
     
     (r'^(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})/$', 'show'),
     (r'^m/(?P<year>\d{4})-(?P<month>\d{2})-(?P<day>\d{2})/$', 'show_m'),
